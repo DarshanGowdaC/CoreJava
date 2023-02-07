ae1edb8b3c9d9e723fdb312f7487318ca422c99d
DarshanGowdaC <118912044+DarshanGowdaC@users.noreply.github.com>
2023-02-07 23:30:04 +0530

    Add files via upload

---
+++ railway-management-system/src/com/xworkz/railway/Tester.java
@@ -0,0 +1,14 @@
+package com.xworkz.railway;
+
+import com.xworkz.railway.railwaystation.Ksr;
+import com.xworkz.railway.railwaystation.RailwayStation ;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		RailwayStation ksr =new RailwayStation();
+		
+		RailwayStation railwayStation = new RailwayStation();
+	}
+	
+}

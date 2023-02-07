81f11c30636f1ddabe0ce454989c1276a2f63ff1
DarshanGowdaC <118912044+DarshanGowdaC@users.noreply.github.com>
2023-02-07 23:17:57 +0530

    Add files via upload

---
+++ Sambra.java
@@ -0,0 +1,14 @@
+package com.xworkz.airport.airport;
+import lombok.Getter;
+
+public class Sambra extends Airport {
+
+	public Sambra() {
+		
+	}
+	public String boarding() {
+		System.out.println("maga boarding ayta ..?");
+	return null;}
+	
+
+}
